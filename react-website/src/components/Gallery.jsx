fd726e184b8202e53c3c8b4b6450a33b48228f96
Zvimarmor <zvi.marmor@mail.huji.ac.il>
2025-09-01 11:24:50 +0300

    Fix mobile gallery photo ordering
    - Reorder photo8.JPG to appear in correct position on mobile layout
    - Add CSS ordering system for different desktop/mobile arrangements
    - Photo8 now appears in bottom-left on mobile instead of bottom-right

--- react-website/src/components/Gallery.jsx
+++ react-website/src/components/Gallery.jsx
@@ -2,15 +2,15 @@ import './Gallery.css'
 
 const Gallery = () => {
   const images = [
-    { src: '/images/intro/photo1.JPG', className: 'tall', alt: '' },
-    { src: '/images/intro/photo5.JPG', className: 'big', alt: '' },
-    { src: '/images/intro/photo3.JPG', className: 'wide', alt: '' },
-    { src: '/images/intro/photo2.JPG', className: '', alt: '' },
-    { src: '/images/intro/photo.JPG', className: '', alt: '' },
-    { src: '/images/intro/photo6.png', className: '', alt: '' },
-    { src: '/images/intro/photo4.png', className: '', alt: '' },
-    { src: '/images/intro/photo7.jpg', className: 'wide', alt: '' },
-    { src: '/images/intro/photo8.JPG', className: '', alt: '' },
+    { src: '/images/intro/photo1.JPG', className: 'tall', alt: '', desktopOrder: 1, mobileOrder: 1 },
+    { src: '/images/intro/photo5.JPG', className: 'big', alt: '', desktopOrder: 2, mobileOrder: 2 },
+    { src: '/images/intro/photo3.JPG', className: 'wide', alt: '', desktopOrder: 3, mobileOrder: 3 },
+    { src: '/images/intro/photo2.JPG', className: '', alt: '', desktopOrder: 4, mobileOrder: 4 },
+    { src: '/images/intro/photo.JPG', className: '', alt: '', desktopOrder: 5, mobileOrder: 5 },
+    { src: '/images/intro/photo6.png', className: '', alt: '', desktopOrder: 6, mobileOrder: 6 },
+    { src: '/images/intro/photo4.png', className: '', alt: '', desktopOrder: 7, mobileOrder: 7 },
+    { src: '/images/intro/photo7.jpg', className: 'wide', alt: '', desktopOrder: 8, mobileOrder: 9 },
+    { src: '/images/intro/photo8.JPG', className: '', alt: '', desktopOrder: 9, mobileOrder: 8 },
   ]
 
   return (
@@ -20,7 +20,7 @@ const Gallery = () => {
           <img 
             key={index}
             src={image.src} 
-            className={image.className} 
+            className={`${image.className} desktop-order-${image.desktopOrder} mobile-order-${image.mobileOrder}`}
             alt={image.alt}
           />
         ))}
